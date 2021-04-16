773b884962e2b1a6b93375262b7b175e5121f095
sukanta7660 <spurkayastha83@gmail.com>
2021-04-16 22:06:46 +0600

    Initial Entry

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
@@ -24,5 +18,52 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.center {
+  margin: auto;
+  width: 50%;
+  border: 3px solid green;
+  padding: 10px;
+  text-align: center;
+  margin-bottom: 10px;
+}
+.btn-main {
+  background-color: #111;
+  color: #fff;
+  padding: 10px;
+  cursor: pointer;
+}
+.btn-main:hover {
+  background-color: rgba(34, 31, 31, 0.74);
+}
+img {
+  border-radius: 50%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  background-color: #11111140;
+}
+.center h3 {
+  margin-bottom: -20px;
+}
+.center h1 {
+  margin-bottom: -20px;
+}
+.center p {
+  margin-top: -20px;
+}
+.form {
+  margin: auto;
+  width: 50%;
+  border: 3px solid rgb(28, 154, 238);
+  padding: 10px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+input[type="text"] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
 }
 </style>
