c3d3dc7d49dd176cbd755e42f1b51e501927fdb7
sukanta7660 <spurkayastha83@gmail.com>
2021-04-21 17:26:38 +0600

    Route added

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,19 @@
 <template>
   <div id="app">
-    
+    <Header/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
-
 <script>
+import Header from "@/components/Shared/Header.vue";
 export default {
-  name: "App",
-};
+  components:{
+    Header
+  }
+}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.center {
-  margin: auto;
-  width: 50%;
-  border: 3px solid green;
-  padding: 10px;
-  text-align: center;
-  margin-bottom: 10px;
-}
-.btn-main {
-  background-color: #111;
-  color: #fff;
-  padding: 10px;
-  cursor: pointer;
-}
-.btn-main:hover {
-  background-color: rgba(34, 31, 31, 0.74);
-}
-img {
-  border-radius: 50%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  background-color: #11111140;
-}
-.center h3 {
-  margin-bottom: -20px;
-}
-.center h1 {
-  margin-bottom: -20px;
-}
-.center p {
-  margin-top: -20px;
-}
-.form {
-  margin: auto;
-  width: 50%;
-  border: 3px solid rgb(28, 154, 238);
-  padding: 10px;
-  margin-bottom: 10px;
-  text-align: center;
-}
 
-input[type="text"] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-}
 </style>
