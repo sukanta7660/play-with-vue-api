a73a310cd272c6fb3a5089ea54928b2a13ee0173
sukanta7660 <spurkayastha83@gmail.com>
2021-04-21 17:28:37 +0600

    Route file & component added

---
+++ src/components/Shared/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <router-link class="navbar-brand" to="/">{{ siteName }}</router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/" active-class="active" exact="">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link active-class="active" to="/about" class="nav-link" exact>About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+export default {
+   data(){
+       return{
+           siteName: "Basic API App"
+       }
+   }
+}
+</script>
+<style scoped>
+
+</style>
