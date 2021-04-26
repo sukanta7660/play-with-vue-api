d7e68c09e5c877e3e5e86c4583efb5f4e3a7e088
sukanta7660 <spurkayastha83@gmail.com>
2021-04-26 20:59:51 +0600

    filter added

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -26,16 +26,39 @@
         </form>
         <h3 class="mt-3">ToDo List</h3>
         <hr />
-        <ul class="list-group todo-list" v-if="todolist.length > 0">
-          <li class="list-group-item d-flex justify-content-between align-items-center" v-for="todo in todolist" :key="todo.id">
+        <ul class="list-group todo-list" v-if="todoFilterd.length > 0">
+          <li class="list-group-item d-flex justify-content-between align-items-center" v-for="todo in todoFilterd" :key="todo.id">
             <input type="checkbox" name="" id="" v-model="todo.isComplete">
             <!-- <input type="checkbox" name="" id="" v-if="todo.isComplete == false"> -->
             <span class="todo-title" :class="{done: todo.isComplete}" @click="doneTodo(todo)">{{todo.title}}</span>
             <span class="badge"
-              ><button class="btn btn-sm btn-danger" @click="removeTodo(todo)">Remove</button></span>
+              >
+              <button class="btn btn-sm btn-danger" @click="removeTodo(todo)">Remove</button></span>
           </li>
         </ul>
-        <h5 v-if="todolist.length == 0">Todo list is Empty.</h5>
+        <h5 v-if="todoFilterd.length == 0">No Items.</h5>
+      </div>
+      <div class="col-md-12"> 
+        <div class="row mt-5 d-flex justify-content-center">
+          <div class="col-md-6 ml-2">
+            <hr>
+            <input type="checkbox" id="checkAll" :checked="!anyRemaining" @change="checkAll">
+            <label for="checkAll"> Check All</label>
+            <span class="float-right">{{remaining}} items left</span>
+          </div>
+        </div>
+        
+      </div>
+      <div class="col-md-12"> 
+        <div class="row mt-5 d-flex justify-content-center">
+          <div class="col-md-6">
+            <button :class="{ active: filter == 'all' }" class="btn btn-sm btn-default mr-1" @click="filter = 'all'">ALL</button>
+            <button :class="{ active: filter == 'active' }" class="btn btn-sm btn-default mr-1" @click="filter = 'active'">All Active</button>
+            <button :class="{ active: filter == 'completed' }" class="btn btn-sm btn-default mr-1" @click="filter = 'completed'">All Completed</button>
+            <button class="btn btn-sm btn-danger pull-right" v-if="clearCompletedBtn" @click="clearCompleted">Clear Completed</button>
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -47,13 +70,37 @@ export default {
   data() {
     return {
       newTodo: '',
+      filter: 'all',
       todolist: []
     }
+  },
+  computed:{
+    remaining(){
+      return this.todolist.filter(todo => !todo.isComplete).length
+    },
+    anyRemaining(){
+      return this.remaining != 0
+    },
+    todoFilterd(){
+      if (this.filter == 'all') {
+        return this.todolist
+      }else if(this.filter == 'active'){
+        return this.todolist.filter(todo => !todo.isComplete)
+        
+      }else if(this.filter == 'completed'){
+        return this.todolist.filter(todo => todo.isComplete)
+      }
+      return this.todolist
+    },
+    clearCompletedBtn(){
+      return this.todolist.filter(todo => todo.isComplete).length > 0
+    }
+    
   },
   methods:{
     addTodo(){
       if (this.newTodo != "") {
-        this.todolist.push({id:1, title: this.newTodo, isComplete: false});
+        this.todolist.push({title: this.newTodo, isComplete: false});
         Swal.fire({
           position: 'top-end',
           icon: 'success',
@@ -89,6 +136,12 @@ export default {
       } else {
         todo.isComplete = true
       }
+    },
+    checkAll(){
+      this.todolist.forEach((todo) => todo.isComplete = true);
+    },
+    clearCompleted(){
+      this.todolist = this.todolist.filter(todo => !todo.isComplete)
     }
   }
 };
@@ -107,6 +160,10 @@ span.todo-title {
 .done {
 		text-decoration: line-through;
 }
+.active, .btn:hover {
+  background-color: #666;
+  color: white;
+}
 ul.list-group.todo-list li {
   margin-bottom: 10px;
   border-top: 1px solid #ccc;
@@ -124,7 +181,6 @@ hr {
   -ms-user-select: none;
   user-select: none;
 }
-
 @media (min-width: 768px) {
   .bd-placeholder-img-lg {
     font-size: 3.5rem;
